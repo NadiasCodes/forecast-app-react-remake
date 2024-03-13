049710ebf3e9abc25df1183cedae6081e4090389
NadiasCodes <De.nadia@outlook.com>
2024-03-13 12:56:11 +0100

    added the api and conditional rendering

--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
 export default function Weather() {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
   function handleResponse(response) {
-    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      feelslike: response.data.temperature.feels_like,
+      temperature: response.data.temperature.current,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+    });
   }
 
-  const apiKey = process.env.REACT_APP_FORECAST_API_KEY;
-  let city = "Paris";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="weather-box">
-      <form>
-        <div className="d-flex justify-content-center m-2 form-box">
-          <input
-            type="search"
-            placeholder="Search for a city..."
-            className="input-search"
-          />
-
-          <input type="submit" value="Search" className="input-button" />
-        </div>
-        <h1 className="city">Berlin</h1>
-        <ul>
-          <li>Monday 13:37</li>
-          <li>Mostly Cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="mostly-cloudy"
-              className="icon"
+  if (weatherData.ready) {
+    return (
+      <div className="weather-box">
+        <form>
+          <div className="d-flex justify-content-center m-2 form-box">
+            <input
+              type="search"
+              placeholder="Search for a city..."
+              className="input-search"
             />
-            <span className="temperature">7</span>
-            <span className="unit">°C</span>
+
+            <input type="submit" value="Search" className="input-button" />
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 0%</li>
-              <li>Humidity: 73%</li>
-              <li>Wind: 18 km/h</li>
-            </ul>
+          <h1 className="city">{weatherData.city}</h1>
+          <ul>
+            <li>Monday 13:37</li>
+            <li>{weatherData.description}</li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="icon"
+              />
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Feels Like: {Math.round(weatherData.feelslike)}°C</li>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
+  } else {
+    const apiKey = process.env.REACT_APP_FORECAST_API_KEY;
+    let city = "Paris";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
